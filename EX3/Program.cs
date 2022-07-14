a661946bf647254d01ad72ee6808ab97326d1729
Gulyutin <gulyutin@gmail.com>
2022-07-14 15:38:16 +0700

    Задача 3

---
+++ EX3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int n = (a % 2);
+
+{
+    if (n == 1) 
+    {
+        Console.Write("Число нечётное");
+    }
+    else
+    {
+        Console.Write("Число чётное");
+    }   
+}
