78a4469de489003c9167c7cdeb5ca44b891a8089
Gulyutin <gulyutin@gmail.com>
2022-07-14 15:45:51 +0700

    Задача 4

---
+++ EX4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while (i <= N)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i = i+2;
+}
